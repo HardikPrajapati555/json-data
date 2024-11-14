2036b5ca5fcb00c01fd5cc55887a45c5fdeed34e
Hardik Prajapati <135410619+HardikPrajapati555@users.noreply.github.com>
2024-11-14 10:44:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const screensData = [
     {
         "screenid": 123456,
         "pairingcode": "123456",
-        "screenname": "User123",
+        "screenname": "emotional",
         "status": false,
         "tags": "a",
         "location": "india",
@@ -15,11 +15,11 @@ const screensData = [
         "state": "india",
         "pincode": "india",
         "country": "india",
-        "slot1": "[\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731322855/kzndi10eklgiufr6p554.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323114/t5drhb39zphrmgf5xjd3.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731131246/jjjabqisfbatnpgqelky.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323381/rvv4vn7qhm4sxakltglq.mp4\"]",
+        "slot1": "https://res.cloudinary.com/dqfnwh89v/video/upload/v1731560708/happy_3_sjpnol.mp4",
   
-        "slot2":"[\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323381/rvv4vn7qhm4sxakltglq.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731322855/kzndi10eklgiufr6p554.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323114/t5drhb39zphrmgf5xjd3.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731131246/jjjabqisfbatnpgqelky.mp4\"]",
-        "slot3": "[\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731131246/jjjabqisfbatnpgqelky.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1730960632/dvyzhk7o80pifhfqrgee.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323114/t5drhb39zphrmgf5xjd3.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323381/rvv4vn7qhm4sxakltglq.mp4\"]",
-        "slot4":  "[\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731131246/jjjabqisfbatnpgqelky.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731322855/kzndi10eklgiufr6p554.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323114/t5drhb39zphrmgf5xjd3.mp4\",\"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731323114/t5drhb39zphrmgf5xjd3.mp4\"]",
+        "slot2":"https://res.cloudinary.com/dqfnwh89v/video/upload/v1731560928/sad_3_bnqrif.mp4",
+        "slot3": "https://res.cloudinary.com/dqfnwh89v/video/upload/v1731560710/men_gjpgvn.mp4",
+        "slot4":  "https://res.cloudinary.com/dqfnwh89v/video/upload/v1731560710/women_j5jglc.mp4",
         "slot5": null,
         "slot6": null,
         "slot7": null,
@@ -30,7 +30,7 @@ const screensData = [
     {
         "screenid": 123654,
         "pairingcode": "123123",
-        "screenname": "User789",
+        "screenname": "TwoD",
         "status": false,
         "tags": "03-10",
         "location": "Ahmedabad",
